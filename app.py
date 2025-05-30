01d3873b5aabd972f9c613d635350348b4bace8b
Abhishek Kumar <abhishek21031@gmail.com>
2025-05-30 23:00:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Good Foodz')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('fooodz.jpg')
+with col2: 
+    st.write("""When writing a description for a food website, you should include information such as your location, cuisine type, and restaurant name. If you are known for a specific dish, it can also be helpful to include the name of that dish. To make a food website effective, it should be visually appealing with high-quality photos and a clean, visually cohesive design. It should also be easy to navigate with a clear hierarchy of information and a logical layout.
+""")
